0cc84be08f531613611a54585b86a3ef6f5a0eed
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-14 16:08:26 +0900

    fix

--- get_next_line.c
+++ get_next_line.c
@@ -12,58 +12,6 @@
 
 #include "get_next_line.h"
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (s[i] != '\0')
-// 		i++;
-// 	return (i);
-// }
-
-
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	size_t	i;
-// 	size_t	len_s;
-
-// 	len_s = ft_strlen(s);
-// 	i = 0;
-// 	while (i < len_s + 1)
-// 	{
-// 		if (s[i] == (char)c)
-// 			return ((char *)(s + i));
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
-
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	size_t	i;
-// 	size_t	len_s1;
-// 	size_t	len_s2;
-// 	char	*ptr;
-
-// 	len_s1 = ft_strlen(s1);
-// 	len_s2 = ft_strlen(s2);
-// 	ptr = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-// 	if (ptr == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (i < len_s1 + len_s2)
-// 	{
-// 		if (i < len_s1)
-// 			ptr[i] = s1[i];
-// 		else
-// 			ptr[i] = s2[i - len_s1];
-// 		i++;
-// 	}
-// 	ptr[i] = '\0';
-// 	return (ptr);
-// }
-
 char	*ft_get_next(char *over)
 {
 	char	*s;
@@ -115,7 +63,6 @@ char	*ft_get_line(char *over)
 char	*ft_readline(int fd, char *over)
 {
 	char	*buf;
-	char	*tmp;
 	ssize_t	size;
 
 	if (!over)
@@ -127,7 +74,10 @@ char	*ft_readline(int fd, char *over)
 	}
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
+	{
+		free(over);
 		return (NULL);
+	}
 	size = 1;
 	while (size > 0 && !ft_strchr(over, '\n'))
 	{
@@ -148,7 +98,8 @@ char	*get_next_line(int fd)
 	static char	*over;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	// if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	over = ft_readline(fd, over);
 	if (!over)
@@ -190,15 +141,6 @@ int main(void)
 		line = get_next_line(fd);
 		printf("%s", line);
 	}
-		// line = get_next_line(fd);
-		// line = get_next_line(fd);
-		// line = get_next_line(fd);
-		// line = get_next_line(fd);
-		// line = get_next_line(fd);
-		// printf("%s", line);
-		// line = get_next_line(fd);
-		// printf("%s", line);
-		// line = get_next_line(fd);
 	close(fd);
 	#endif
 
